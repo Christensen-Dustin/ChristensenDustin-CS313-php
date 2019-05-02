2ecf34eacc3e32146fec4efb7c4564c80b751190
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-01 22:15:08 -0700

    second update for Prove 02

--- web/Prove Assignments/interest.php
+++ web/Prove Assignments/interest.php
@@ -6,13 +6,30 @@
     <meta charset="utf-8" />
 </head>
 <body>
-    <ul class="topmenu">
+    <!--<ul class="topmenu">
         <li <?php if($_SERVER['SCRIPT_NAME']=="/home.php") { ?>  class="active"   <?php   }  ?>><a href="home.php"><b>Bienvenue</b></a></li>
         <li <?php if($_SERVER['SCRIPT_NAME']=="/livres.php") { ?>  class="active"   <?php   }  ?>><a href="livres.php"><b>Livres</b></a></li>
         <li <?php if($_SERVER['SCRIPT_NAME']=="/bibliotheque.php") { ?>  class="active"   <?php   }  ?>><a href="bibliotheque.php"><b>Bibliothèque</b></a></li>
         <li <?php if($_SERVER['SCRIPT_NAME']=="/universite.php") { ?>  class="active"   <?php   }  ?>><a href="universite.php"><b>L'université</b></a></li>
         <li <?php if($_SERVER['SCRIPT_NAME']=="/contact.php") { ?>  class="active"   <?php   }  ?>><a href="contact.php"><b>Contact</b></a></li>
-    </ul>
+    </ul>-->
+    
+    <?php
+    $pages = scandir('pages-php');
+    $hidden = array('.', '..');
+    
+    echo '<div class="menubar">';
+    
+    foreach($pages as $page) {
+        if(!in_array($page, $hidden)) {
+            $link = 'pages-php' . $page;
+            $cleanName = strtoupper(str_replace('.php', '', $page));
+            echo'<a href="' . $link . '">' . $cleanName . '</a> <br>';
+        } // close if statement
+     } // close foreach
+    
+    echo '</div>';
+    ?>
         
 </body>
 </html>
