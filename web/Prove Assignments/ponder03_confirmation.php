ca0dd8e7a715123e3e71d2c6a7fa6ef4f1b33bae
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-09 20:32:01 -0700

    Seventh Update for Prove03

---
+++ web/Prove Assignments/ponder03_confirmation.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="eng-US">
+<head>
+    <title> Confirmation.php </title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <script type="text/javascript" src="name of file.js"></script>
+</head>
+<body>
+<header>
+    <h1>Confirmation of YOUR order</h1>
+</header>
+<div class="navBar">
+    <a class="active" href="index.html">Home</a>
+    <a href="interest.php"> Other Interest</a>
+    <a href="assignment.html">Assignments</a>
+    <a id="cart" href="ponder03.php">Browse ITEMS</a>
+</div>
+<br>
+<br>
+    
+        
+</body>
+</html>
