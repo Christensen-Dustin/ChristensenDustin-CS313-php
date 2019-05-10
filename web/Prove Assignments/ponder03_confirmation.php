bc779d73e1c2d390b76ec3629b5dc4eb67c94bc2
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-09 21:46:49 -0700

    nineth Update for Prove03

--- web/Prove Assignments/ponder03_confirmation.php
+++ web/Prove Assignments/ponder03_confirmation.php
@@ -22,7 +22,66 @@
 </div>
 <br>
 <br>
-    
-        
+<h1>Confirmation of your order</h1>
+<p>Your name: <?=$_SESSION['fname'] ?> <?=$_SESSION['lname'] ?></p>
+    <br>
+<p>   Street: <?=$_SESSION['stNum'] ?> <?=$_SESSION['StName'] ?></p>
+    <br>
+<p>     City: <?=$_SESSION['city'] ?> State: <?=$_SESSION['state'] ?> 
+    Zip: <?=$_SESSION['zip'] ?></p>
+    <br>
+<p>Special Instructions:<br>
+    <?=$_SESSION['special'] ?></p>
+    <br>
+    <br>
+<?php
+if($_SESSION['isSk'] == true) {
+        echo
+"<table class='cart'>
+    <tr class='info'>
+        <th id='thCart'>
+            <img class='thumbPic' src='survival_knife.jpg' alt='Survival Knife'>
+        </th>
+        <td id='tdCart'>" . $_SESSION['sk'] . "</td>
+        <td id='tdCart'>Quantity: " . $_SESSION['sk_qty'] . "</td>
+    </tr>
+</table>"; }
+    if($_SESSION['isFs'] == true) {
+        echo
+"<table class='cart'>
+    <tr class='info'>
+        <th id='thCart'>
+            <img class='thumbPic' src='flint_steel.jpg' alt='Flint and Steel'>
+        </th>
+        <td id='tdCart'>" . $_SESSION['fs'] . "</td>
+        <td id='tdCart'>Quantity: " . $_SESSION['fs_qty'] . "</td>
+    </tr>
+</table>"; }
+    if($_SESSION['isHp'] == true) {
+        echo
+"<table class='cart'>
+    <tr class='info'>
+        <th id='thCart'>
+            <img class='thumbPic' src='water_pack.jpg' alt='Hydration Pack'>
+        </th>
+        <td id='tdCart'>" . $_SESSION['hp'] . "</td>
+        <td id='tdCart'>Quantity: " . $_SESSION['hp_qty'] . "</td>
+    </tr>
+</table>"; }
+    if($_SESSION['isWp'] == true) {
+        echo
+"<table class='cart'>
+    <tr class='info'>
+        <th id='thCart'>
+            <img class='thumbPic' src='water_purifier.jpg' alt='Portable Water Purifier'>
+        </th>
+        <td id='tdCart'>" . $_SESSION['wp'] . "</td>
+        <td id='tdCart'>Quantity: " . $_SESSION['wp_qty'] . "</td>
+    </tr>
+</table>"; }
+
+
+
+?>        
 </body>
 </html>
