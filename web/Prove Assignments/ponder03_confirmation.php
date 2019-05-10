e6a9728df790adff5113ae25a68b5caed786694b
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-09 22:50:48 -0700

    nineth Update for Prove03

--- web/Prove Assignments/ponder03_confirmation.php
+++ web/Prove Assignments/ponder03_confirmation.php
@@ -30,7 +30,7 @@
 </div>
 <br>
 <br>
-<h1 class="client">Confirmation of your order</h1>
+<h1 class="conf">Confirmation of your order</h1>
 <?php
 echo
     "<p>Your name: " . $fname . " " . $lname . "</p>
@@ -38,6 +38,8 @@ echo
     <p>City: " . $city . "  -  State: " . $state . "  -  Zip: " . $zip . "</p>
     <p>Special Instructions:<br>" . $special . "</p>
     <br>";
+
+echo "<h1 class='conf'>Items ordered</h1>"
 
 if($_SESSION['isSk'] == true) {
         echo
@@ -48,9 +50,6 @@ if($_SESSION['isSk'] == true) {
         </th>
         <td id='tdCart'>" . $_SESSION['sk'] . "</td>
         <td id='tdCart'>Quantity: " . $_SESSION['sk_qty'] . "</td>
-        <td id='tdCart'><form method='post' action='ponder03_cart.php'>
-        <input type='submit' value='REMOVE' name='removeSk'>
-        </form></td>
     </tr>
 </table>"; }
     if($_SESSION['isFs'] == true) {
