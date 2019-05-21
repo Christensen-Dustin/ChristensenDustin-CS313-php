8f2e0fce272afbb335427a0b156c303a820f55c0
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-20 17:15:25 -0700

    Prepare05 Second update

--- web/Prepare Assignments/prepare05/connectDB.php
+++ web/Prepare Assignments/prepare05/connectDB.php
@@ -10,9 +10,9 @@ try
     $dbPort = $dbOpts["port"];
     $dbUser = $dbOpts["user"];
     $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
+    $dbName = ltrim($dbOpts["path"], '/');
     
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPost;dbname=$dbName", $dbUser, $dbPassword);
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbName=$dbName", $dbUser, $dbPassword);
     
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
