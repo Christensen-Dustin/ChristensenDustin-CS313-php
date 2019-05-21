8f4f4310e4e72cbecbc3918a8ec15028bf73701b
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-20 17:43:45 -0700

    Prepare05 third update

--- web/Prepare Assignments/prepare05/prepare05.php
+++ web/Prepare Assignments/prepare05/prepare05.php
@@ -33,7 +33,17 @@ $statement2 = $db->query('select username, password, from note_user');
 $results = $statement2->fetchAll(PDO::FETCH_ASSOC);
 
 
-
+// Prepared statements style one
+$stmt = $db->prepare('select * from table where id=:id and name=:name');
+&stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$row3 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+// Prepared statements style two
+$stmt2 = $db->prepare('select * from table where id=:id and name=:name');
+$stmt2->execute(array(':name' => $name, ':id' => $id));
+$row4 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 
 
 
