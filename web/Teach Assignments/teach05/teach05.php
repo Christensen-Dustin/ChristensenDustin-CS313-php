91da788fcd78acc9f90a5db654d98f08043c1755
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-20 19:27:21 -0700

    Teach 05 Second Update

--- web/Teach Assignments/teach05/teach05.php
+++ web/Teach Assignments/teach05/teach05.php
@@ -15,7 +15,7 @@ include 'connectDB.php';
     echo '<h1>Scripture Resources</h1>';
     foreach ($db->query('select book, chapter, verse, content from scriptures') as $row)
     {
-        echo $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . ' - "' . $row[content] .'"';
+        echo $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . ' - "' . $row[content] .'" <br/>';
     }
 
 
