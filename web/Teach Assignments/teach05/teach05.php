30867aa630fcd76a9fda9454c62aa5f8279a1ca9
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-20 18:44:55 -0700

    Teach 05 first load

---
+++ web/Teach Assignments/teach05/teach05.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html> 
+<html lang="eng-US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Scripture Resource</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<script type="text/javascript" src="name of file.js"></script>
+	<style></style>
+</head>  
+<body>
+<?php
+include 'connectDB.php';
+
+
+
+?>
+</body>
+</html>
