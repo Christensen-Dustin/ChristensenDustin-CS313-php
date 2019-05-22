908aa7a33659c4b4c081b3139badaa8dc84bef32
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-21 21:31:15 -0700

    Teach 05 Fourth update

--- web/Teach Assignments/teach05/teach05.php
+++ web/Teach Assignments/teach05/teach05.php
@@ -1,4 +1,5 @@
 <?php
+        session_start();
         $name = $_GET['book'];
 ?>
 <!DOCTYPE html> 
@@ -13,12 +14,12 @@
 include 'connectDB.php';
     echo "name = " . $name;
     echo '<h1>Scripture Resources</h1>';
-    foreach ($db->query("select book, chapter, verse, content from scriptures where book = '$name'") as $row)
+    foreach ($db->query("select book, chapter, verse, id from scriptures where book = '$name'") as $row)
     {
         echo '<b>';
         echo $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . ' - "';
-        echo '</b>';
-        echo $row[content] .'" <br/><br/>';
+        $details = $row['id'];
+        echo "</b><a href='teach05_details.php?id=$details'>click here for details</a>";
     }
 echo "name = " . $name;
     
--- web/Teach Assignments/teach05/teach05_details.php
+++ web/Teach Assignments/teach05/teach05_details.php
@@ -1,23 +1,26 @@
 <?php
-
+        session_start();
+        $name = $_GET['id'];
 ?>
 <!DOCTYPE html> 
 <html lang="eng-US">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Scripture Details</title>
-	<link rel="stylesheet" type="text/css" href="index.css">
-	<script type="text/javascript" src="name of file.js"></script>
+	<title>Scripture Resource</title>
 	<style></style>
 </head>  
 <body>
 <?php
 include 'connectDB.php';
-    echo '<h1>Scripture Details</h1>';
+    echo "name = " . $name;
+    echo '<h1>Scripture Resources</h1>';
+    foreach ($db->query("select book, chapter, verse, content from scriptures where book = '$name'") as $row)
+    {
+        echo '<b>';
+        echo $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b>- "';
+        echo $row['content'] . '"';
+    }
     
-
-
-
 ?>
 </body>
 </html>
