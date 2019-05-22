3d6313fcb4b335fdbf44f16dca07ed8a925d68ba
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-21 21:07:11 -0700

    Teach 05 Fourth update

--- web/Teach Assignments/teach05/teach05.php
+++ web/Teach Assignments/teach05/teach05.php
@@ -6,13 +6,12 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Scripture Resource</title>
-	<link rel="stylesheet" type="text/css" href="index.css">
-	<script type="text/javascript" src="name of file.js"></script>
 	<style></style>
 </head>  
 <body>
 <?php
 include 'connectDB.php';
+    echo "name = " . $name;
     echo '<h1>Scripture Resources</h1>';
     foreach ($db->query("select book, chapter, verse, content from scriptures where book = $name") as $row)
     {
