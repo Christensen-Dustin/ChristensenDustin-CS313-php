ea0aacaea8429964186fcb603c00bd68c849f475
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-23 16:09:51 -0700

    Project DB Update

---
+++ db/trialFamilyDB3.sql
@@ -0,0 +1,33 @@
+-- Steps Table Trial Information
+insert into steps (steps_details, steps_done, steps_goal-fk)
+    values ('Save money', 0, );
+insert into steps (steps_details, steps_done, steps_goal-fk)
+    values ('Make reservations', 0, );
+insert into steps (steps_details, steps_done, steps_goal-fk)
+    values ('Pack lugage', 0, );
+insert into steps (steps_details, steps_done, steps_goal-fk)
+    values ('Place hold on mail', 0, );
+insert into steps (steps_details, steps_done, steps_goal-fk)
+    values ('Ask Carol to watch Lucy', 0, );
+insert into steps (steps_details, steps_done, steps_goal-fk)
+    values ('Save money', 0, );
+
+-- Family Table Trial Information
+insert into family
+(family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values ();
+insert into family
+(family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values ();
+insert into family
+(family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values ();
+insert into family
+(family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values ();
+insert into family
+(family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values ();
+insert into family
+(family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values ();
