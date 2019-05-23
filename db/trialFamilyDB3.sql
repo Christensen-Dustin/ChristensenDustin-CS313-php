8a9ead7a3e6dd8d725451b0c3ae397e5e049ff8f
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-23 16:44:34 -0700

    Project DB update

--- db/trialFamilyDB3.sql
+++ db/trialFamilyDB3.sql
@@ -1,73 +1,73 @@
 -- Steps Table Trial Information
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Save money', false, 3);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Make reservations', false, 3);
 insert into steps (steps_details, steps_done, steps_goal-fk)
     values ('Pack lugage', false, 3);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Place hold on mail', false, 3);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Ask Carol to watch Lucy', false, 3);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Save money', false, 2);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Call relatives', false, 2);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Plan dinner', false, 2);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Assign Side Dishes', false, 2);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Arrange for additional chairs and tables', false, 2);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('See what is playing', false, 1);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Select Concerts', false, 1);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Save money', false, 1);
-insert into steps (steps_details, steps_done, steps_goal-fk)
+insert into steps (steps_details, steps_done, steps_goal_fk)
     values ('Order Tickets', false, 1);
 
 -- Family Table Trial Information
 -- Chores
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (1, 0, 0, 1, 2, 1);
+    values (1, , , 1, 2, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (2, 0, 0, 1, 1, 1);
+    values (2, , , 1, 1, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (3, 0, 0, 0, 2, 1);
+    values (3, , , , 2, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (4, 0, 0, 0, 2, 1);
+    values (4, , , , 2, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (5, 0, 0, 0, 1, 1);
+    values (5, , , , 1, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (6, 0, 0, 1, 2, 1);
+    values (6, , , 1, 2, 1);
 
 -- Rewards
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (0, 0, 1, 2, 2, 1);
+    values (, , 1, 2, 2, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (0, 0, 2, 2, 1, 1);
+    values (, , 2, 2, 1, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (0, 0, 3, 2, 2, 1);
+    values (, , 3, 2, 2, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (0, 0, 4, 1, 1, 1);
+    values (, , 4, 1, 1, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (0, 0, 5, 0, 1, 1);
+    values (, , 5, , 1, 1);
 insert into family
 (family_chore_fk, family_goal_fk, family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
-    values (0, 0, 6, 0, 2, 1);
+    values (, , 6, , 2, 1);
 
 -- Goal
 -- Add to another file, if it is needed.
