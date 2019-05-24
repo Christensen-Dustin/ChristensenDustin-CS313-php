2e0bf28e1022f7e8336ee9a2332dea1804a0c55b
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-24 00:33:55 -0700

    Project Website Update 2

--- web/Prove Assignments/familyChore_users.php
+++ web/Prove Assignments/familyChore_users.php
@@ -2,7 +2,7 @@
     include 'connectDB.php';
     session_start();
     $result = $_GET['userName'];
-    $expanded = ('|', $result);
+    $expanded = explode('|', $result);
     $id = $expanded[0];
     $display = $expanded[1];
 ?>
@@ -24,13 +24,13 @@
 <h1>User Details</h1>
 <?php
 
-foreach ($db->query("select parent_fname, parent_lname, parent_bday, parent_display from parent where parent_pk='$display'") as $rowParent)
+foreach ($db->query("select parent_fname, parent_lname, parent_bday, parent_display from parent where parent_display='$display'") as $rowParent)
 {
     echo 'User Name: ' . $rowParent['parent_fname'] . ' ' . $rowParent['parent_lname'] . '</br>';
     echo 'Birthday: ' . $rowParent['parent_bday'] . '</br>'; 
 }
 
-foreach ($db->query("select child_fname, child_lname, child_bday, child_display from child where child_pk='$display'") as $rowChild)
+foreach ($db->query("select child_fname, child_lname, child_bday, child_display from child where child_display='$display'") as $rowChild)
 {
     echo 'User Name: ' . $rowChild['child_fname'] . ' ' . $rowChild['child_lname'] . '</br>';
     echo 'Birthday: ' . $rowChild['child_bday'] . '</br>';
