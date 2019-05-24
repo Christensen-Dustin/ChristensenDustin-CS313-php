47df8cdba36d89144a6d82d493347e702e4db25b
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-24 01:36:54 -0700

    Project DB Update 3

--- web/Prove Assignments/familyChore_users.php
+++ web/Prove Assignments/familyChore_users.php
@@ -41,7 +41,7 @@ foreach ($db->query("select child_fname, child_lname, child_bday, child_display 
 
 echo '<h1> Related Details  </h1>';
 echo '<h2> Chores </h2>';
-foreach ($db->query("select parent_pk, parent_display, family_pk, chore_name, family_chore_fk, family_parent_fk from parent, chore, family where parent_display='$display' and family_pk = '$id'") as $rowFamily)
+foreach ($db->query("select parent_pk, parent_display, family_pk, chore_name, family_chore_fk, family_parent_fk from parent, chore, family where parent_display='$display' and family_parent_fk = '$id'") as $rowFamily)
 {
     echo $rowFamily['chore_name'] . '</br>';
 }
