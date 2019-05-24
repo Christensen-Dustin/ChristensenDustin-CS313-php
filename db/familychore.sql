c4d3f8ac8c75a5c9202b8951abf627f9e8b23672
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-23 17:06:00 -0700

    Project Database Update

--- db/familychore.sql
+++ db/familychore.sql
@@ -69,12 +69,12 @@ create table steps (
 -- Family Table
 create table family (
     family_pk           serial not null primary key,                -- family ID
-    family_chore_fk     int not null references chore(chore_pk),    -- reference chore ID
-    family_goal_fk      int not null references goal(goal_pk),      -- reference goal ID
-    family_reward_fk    int not null references reward(reward_pk),  -- reference reward ID
-    family_parent_fk    int not null references parent(parent_pk),  -- reference parent ID
-    family_child_fk     int not null references child(child_pk),    -- reference child ID
-    family_account_fk   int not null references account(account_pk) -- reference account ID
+    family_chore_fk     int references chore(chore_pk),    -- reference chore ID
+    family_goal_fk      int references goal(goal_pk),      -- reference goal ID
+    family_reward_fk    int references reward(reward_pk),  -- reference reward ID
+    family_parent_fk    int references parent(parent_pk),  -- reference parent ID
+    family_child_fk     int references child(child_pk),    -- reference child ID
+    family_account_fk   int references account(account_pk) -- reference account ID
 );
 
 
--- db/trialFamilyDB.sql
+++ db/trialFamilyDB.sql
@@ -19,3 +19,130 @@ insert into child
 insert into child
     (child_fname, child_lname, child_bday, child_display, child_password)
         values ('Lucy', 'Christensen', '2018-11-08', 'Little Dog', 'schooloflife');
+-- Chore Table Trial Information
+insert into chore
+(chore_name, chore_details, chore_expire, chore_repeat, chore_done, chore_date)
+    values
+    ('Walk the dog', 'Take Lucy on a walk around the block', false, true, false, '2019-05-23');
+insert into chore
+(chore_name, chore_details, chore_expire, chore_repeat, chore_done, chore_date)
+    values
+    ('Wash laundry', 'All laundry has to be washed, folded, and put away', true, true, false, '2019-05-25');
+insert into chore
+(chore_name, chore_details, chore_expire, chore_repeat, chore_done, chore_date)
+    values
+    ('Sweep Patio', 'Patio must be swept and organized', true, true, false, '2019-05-25');
+insert into chore
+(chore_name, chore_details, chore_expire, chore_repeat, chore_done, chore_date)
+    values
+    ('Feed Lucy', 'Make sure Lucy gets fed and fresh water', true, true, false, '2019-05-23');
+insert into chore
+(chore_name, chore_details, chore_expire, chore_repeat, chore_done, chore_date)
+    values
+    ('Wash dishes', 'Dishes must be washed after the last meal of the day', true, true, false, '2019-05-23');
+insert into chore
+(chore_name, chore_details, chore_expire, chore_repeat, chore_done, chore_date)
+    values
+    ('Clean Bathroom', 'Tub, Toilet, Counter, mirror and floor must be cleaned', false, true, false, '2019-05-25');
+
+-- Reward Table Trial Information
+insert into reward
+(reward_name, reward_details, reward_expire, reward_repeat, reward_done, reward_date)
+    values
+    ('Pick the Movie', 'Gets to pick the movie the family watches for family night', false, true, false, '2019-08-23');
+insert into reward
+(reward_name, reward_details, reward_expire, reward_repeat, reward_done, reward_date)
+    values
+    ('Pick a restaurant', 'Gets to pick the restaurant for the next family meal out', false, true, false, '2019-06-23');
+insert into reward
+(reward_name, reward_details, reward_expire, reward_repeat, reward_done, reward_date)
+    values
+    ('Pick a game', 'Gets to pick the game the family plays for family night', false, true, false, '2019-07-23');
+insert into reward
+(reward_name, reward_details, reward_expire, reward_repeat, reward_done, reward_date)
+    values
+    ('Pass a chore', 'Gets to pass off a single chore, one time, to another family member', false, true, false, '2019-06-23');
+insert into reward
+(reward_name, reward_details, reward_expire, reward_repeat, reward_done, reward_date)
+    values
+    ('Breakfast with a Parent', 'Gets to go out to breakfast with parent of choice', false, true, false, '2019-07-23');
+insert into reward
+(reward_name, reward_details, reward_expire, reward_repeat, reward_done, reward_date)
+    values
+    ('Lunch with a Parent', 'Gets to go out to lunch with a parent of choice', false, true, false, '2019-09-23');
+
+-- Goal Table Trial Information
+insert into goal
+(goal_name, goal_details, goal_expire, goal_repeat, goal_done, goal_date)
+    values
+    ('Hollywood Bowl Concerts', 'Buying packs of tickets for the summer concert season', true, false, true, '2019-06-07');
+insert into goal
+(goal_name, goal_details, goal_expire, goal_repeat, goal_done, goal_date)
+    values
+    ('Thanksgiving Dinner', 'Plan on hosting the dinner this year', true, false, false, '2019-11-28');
+insert into goal
+(goal_name, goal_details, goal_expire, goal_repeat, goal_done, goal_date)
+    values
+    ('Family Vacation', 'Annual trip to Washington State', true, true, false, '2019-06-13');
+
+-- Steps Table Trial Information
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Save money', false, 3);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Make reservations', false, 3);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Pack lugage', false, 3);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Place hold on mail', false, 3);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Ask Carol to watch Lucy', false, 3);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Save money', false, 2);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Call relatives', false, 2);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Plan dinner', false, 2);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Assign Side Dishes', false, 2);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Arrange for additional chairs and tables', false, 2);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('See what is playing', false, 1);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Select Concerts', false, 1);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Save money', false, 1);
+insert into steps (steps_details, steps_done, steps_goal_fk)
+    values ('Order Tickets', false, 1);
+
+-- Family Table Trial Information
+-- Chores
+insert into family (family_chore_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values (1, 1, 2, 1);
+insert into family (family_chore_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values (2, 1, 1, 1);
+insert into family (family_chore_fk, family_child_fk, family_account_fk)
+    values (3, 2, 1);
+insert into family (family_chore_fk, family_child_fk, family_account_fk)
+    values (4, 2, 1);
+insert into family (family_chore_fk, family_child_fk, family_account_fk)
+    values (5, 1, 1);
+insert into family (family_chore_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values (6, 1, 2, 1);
+
+-- Rewards
+insert into family (family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values (1, 2, 2, 1);
+insert into family (family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values (2, 2, 1, 1);
+insert into family (family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values (3, 2, 2, 1);
+insert into family (family_reward_fk, family_parent_fk, family_child_fk, family_account_fk)
+    values (4, 1, 1, 1);
+insert into family (family_reward_fk, family_child_fk, family_account_fk)
+    values (5, 1, 1);
+insert into family (family_reward_fk, family_child_fk, family_account_fk)
+    values (6, 2, 1);
+
+-- Goal
+-- Add to another file, if it is needed.
