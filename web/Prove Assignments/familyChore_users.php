9ee863c0d3c2d8f349b72c2128d2cd05a27b852d
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-24 00:28:04 -0700

    Project Website Update 2

--- web/Prove Assignments/familyChore_users.php
+++ web/Prove Assignments/familyChore_users.php
@@ -1,7 +1,10 @@
 <?php
     include 'connectDB.php';
     session_start();
-    $userName = $_GET['userName'];
+    $result = $_GET['userName'];
+    $expanded = ('|', $result);
+    $id = $expanded[0];
+    $display = expanded[1];
 ?>
 <!DOCTYPE html> 
 <html lang="eng-US">
@@ -21,13 +24,13 @@
 <h1>User Details</h1>
 <?php
 
-foreach ($db->query("select parent_pk, parent_fname, parent_lname, parent_bday, parent_display from parent where parent_pk='$userName'") as $rowParent)
+foreach ($db->query("select parent_fname, parent_lname, parent_bday, parent_display from parent where parent_pk='$display'") as $rowParent)
 {
     echo 'User Name: ' . $rowParent['parent_fname'] . ' ' . $rowParent['parent_lname'] . '</br>';
     echo 'Birthday: ' . $rowParent['parent_bday'] . '</br>'; 
 }
 
-foreach ($db->query("select child_pk, child_fname, child_lname, child_bday, child_display from child where child_pk='$userName'") as $rowChild)
+foreach ($db->query("select child_fname, child_lname, child_bday, child_display from child where child_pk='$display'") as $rowChild)
 {
     echo 'User Name: ' . $rowChild['child_fname'] . ' ' . $rowChild['child_lname'] . '</br>';
     echo 'Birthday: ' . $rowChild['child_bday'] . '</br>';
@@ -35,7 +38,7 @@ foreach ($db->query("select child_pk, child_fname, child_lname, child_bday, chil
 
 echo '<h1> Related Details  </h1>';
 echo '<h2> Chores </h2>';
-foreach ($db->query("select parent_pk, parent_display, family_pk, chore_name, family_chore_fk, family_parent_fk from parent, chore, family where parent_display='$userName' and parent_pk = family_parent_fk") as $rowFamily)
+foreach ($db->query("select parent_pk, parent_display, family_pk, chore_name, family_chore_fk, family_parent_fk from parent, chore, family where parent_display='$display' and family_fk = '$id'") as $rowFamily)
 {
     echo $rowFamily['chore_name'] . '</br>';
 }
