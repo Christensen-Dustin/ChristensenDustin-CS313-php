8d8c448c721f29c7a0b5f9f6ff93fd7f38ea2f29
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-23 23:03:21 -0700

    Project Website Update 2

--- web/Prove Assignments/familyChore_users.php
+++ web/Prove Assignments/familyChore_users.php
@@ -20,13 +20,13 @@
 
 <h1>User Details</h1>
 <?php
-foreach ($db->query("select parent_fname, parent_lname, parent_bday, parent_display from parent where parent_pk='$userName'") as $rowParent)
+foreach ($db->query("select parent_pk, parent_fname, parent_lname, parent_bday, parent_display from parent where parent_pk='$userName'") as $rowParent)
 {
     echo 'User Name: ' . $rowParent['parent_fname'] . ' ' . $rowParent['parent_lname'] . '</br>';
     echo 'Birthday: ' . $rowParent['parent_bday'] . '</br>'; 
 }
 
-foreach ($db->query("select child_fname, child_lname, child_bday, child_display from child where child_pk='$userName'") as $rowChild)
+foreach ($db->query("select child_pk, child_fname, child_lname, child_bday, child_display from child where child_pk='$userName'") as $rowChild)
 {
     echo 'User Name: ' . $rowChild['child_fname'] . ' ' . $rowChild['child_lname'] . '</br>';
     echo 'Birthday: ' . $rowChild['child_bday'] . '</br>';
