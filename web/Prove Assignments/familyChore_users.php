eec924aa4004e92b4e610a9bf51e085642bb84b0
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-24 00:02:59 -0700

    Project Website Update 2

--- web/Prove Assignments/familyChore_users.php
+++ web/Prove Assignments/familyChore_users.php
@@ -21,33 +21,21 @@
 <h1>User Details</h1>
 <?php
 
-$family_fk;
-
-foreach ($db->query("select parent_fname, parent_lname, parent_bday, parent_display from parent where parent_display='$userName'") as $rowParent)
+foreach ($db->query("select parent_fname, parent_lname, parent_bday, parent_display from parent where parent_pk='$userName'") as $rowParent)
 {
     echo 'User Name: ' . $rowParent['parent_fname'] . ' ' . $rowParent['parent_lname'] . '</br>';
     echo 'Birthday: ' . $rowParent['parent_bday'] . '</br>'; 
-    
-    if(!$rowParent['parent_display'])
-    {
-        global $family_hk = $rowParent['parent_pk'];
-    }
 }
 
-foreach ($db->query("select child_fname, child_lname, child_bday, child_display from child where child_display='$userName'") as $rowChild)
+foreach ($db->query("select child_fname, child_lname, child_bday, child_display from child where child_pk='$userName'") as $rowChild)
 {
     echo 'User Name: ' . $rowChild['child_fname'] . ' ' . $rowChild['child_lname'] . '</br>';
     echo 'Birthday: ' . $rowChild['child_bday'] . '</br>';
-    
-    if(!$rowChild['child_display'])
-    {
-        global $family_fk = $rowChild['child_pk'];
-    }
 }
 
 echo '<h1> Related Details  </h1>';
 echo '<h2> Chores </h2>';
-foreach ($db->query("select parent_pk, parent_display, family_pk, chore_name, family_chore_fk, family_child_fk from parent, chore, family where parent_display='$userName' and family_child_fk ='$family_fk'") as $rowFamily)
+foreach ($db->query("select parent_pk, parent_display, family_pk, chore_name, family_chore_fk, family_parent_fk from parent, chore, family where parent_display='$userName' and parent_pk = family_parent_fk") as $rowFamily)
 {
     echo $rowFamily['chore_name'] . '</br>';
 }
