e2c8eff40ccf3fa8f1b2d66c35fba03eac6b2260
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-28 22:47:12 -0700

    Teach06 fourth Push

--- db/scriptures2.sql
+++ db/scriptures2.sql
@@ -1,7 +1,7 @@
 -- Database for topic
 
 create table topics (
-    id      serial not null primary key,    -- topic ID
+    t_id    serial not null primary key,    -- topic ID
     name    varchar(20) not null            -- Faith, Sacrifice, Charity
 );
 
@@ -12,7 +12,7 @@ insert into topics (name) values ('Charity');
 -- Database for scriptures
 
 create table scriptures (
-    id      serial not null primary key,    -- scripture ID
+    s_id    serial not null primary key,    -- scripture ID
     book    varchar(80) not null,           -- Book of scripture
     chapter int not null,                   -- Chapter with in the book
     verse   int not null,                   -- Verse with in the chapter
@@ -27,13 +27,15 @@ insert into scriptures (book, chapter, verse, content)
     values ('Doctrine and Covenants', 93, 28, 'He that keepeth his commandments receiveth truth and light, until he is glorified in the truth and knoweth all things.');
 insert into scriptures (book, chapter, verse, content)
     values ('Mosiah', 16, 9, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
-    
-    -- Many-to-many Database
+insert into scriptures (book, chapter, verse, content)
+    values ('Hebrews', 11, 4, ' By faith Abel offered unto God a more excellent sacrifice than Cain, by which he obtained witness that he was righteous, God testifying of his gifts: and by it he being dead yet speaketh.');
+
+-- Many-to-many Database
     
 create table topicLinks (
-    id          serial not null primary key,                -- link ID
-    topics_fk   int references topics(id),         -- topic foreign key
-    script_fk   int references scriptures(id)      -- scripture foreign key
+    tl_id       serial not null primary key,                -- link ID
+    topics_fk   int references topics(t_id),         -- topic foreign key
+    script_fk   int references scriptures(s_id)      -- scripture foreign key
 );
 
 INSERT INTO topicLinks (topics_fk, script_fk) VALUES (1, 5);
