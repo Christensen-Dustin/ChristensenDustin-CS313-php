8a07af52f0fee1def933fee261ecb73ba1e755b0
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-30 20:59:48 -0700

    Update Reward to Chore Tracker

--- web/Prove Assignments/familyChore_insert2.php
+++ web/Prove Assignments/familyChore_insert2.php
@@ -44,7 +44,7 @@ if (isset($_POST['parent_pks']))
     foreach ($parent_pks as $parent_pk)
     {
         $stmtFamily = $db->prepare('INSERT INTO family(family_reward_fk,     family_parent_fk, family_account_fk)
-            VALUES (:family_reward_fk, :family_parent_fk, :familiy_account_fk);');
+            VALUES (:family_reward_fk, :family_parent_fk, :family_account_fk);');
         $stmtFamily->bindValue(':family_reward_fk',  $reward_pk, PDO::PARAM_INT);
         $stmtFamily->bindValue(':family_parent_fk',  $parent_pk, PDO::PARAM_INT);
         $stmtFamily->bindValue(':family_account_fk', $account_pk, PDO::PARAM_INT);
