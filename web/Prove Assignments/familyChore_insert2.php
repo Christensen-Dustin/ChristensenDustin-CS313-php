08e663ebd337c774ecae6a103d61c4eadecd4dba
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-30 20:57:45 -0700

    Update Reward to Chore Tracker

--- web/Prove Assignments/familyChore_insert2.php
+++ web/Prove Assignments/familyChore_insert2.php
@@ -39,7 +39,7 @@ foreach ($db->query("SELECT reward_pk FROM reward where reward_name='$reward_nam
 }
     
 // Create New family entry for parent
-if (isset($_POST['parent_pk']))
+if (isset($_POST['parent_pks']))
 {
     foreach ($parent_pks as $parent_pk)
     {
@@ -53,7 +53,7 @@ if (isset($_POST['parent_pk']))
 }
     
 // Create New family entry for child
-if (isset($_POST['child_pk']))
+if (isset($_POST['child_pks']))
 {
     foreach ($child_pks as $child_pk)
     {
