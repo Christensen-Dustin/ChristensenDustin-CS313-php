ac388a8af5ea3ac6b019e0238ad4ffc9ec32a2fa
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-05-30 20:34:43 -0700

    Family reward page

--- web/Prove Assignments/familyChore_insert.php
+++ web/Prove Assignments/familyChore_insert.php
@@ -45,21 +45,17 @@ if (isset($_POST['parent_pks']))
     
     $count = 0;
     
-    //foreach ($parent_pks as $parent_pk)
-    //{
-        //$stmt = $db->prepare('INSERT INTO family(family_chore_fk, family_parent_fk, family_account_fk) VALUES (:family_chore_fk, :family_parent_fk, :family_account_fk);')
-    
     foreach ($parent_pks as $parent_pk)
     {
-        $stmtFamily = $db->prepare('INSERT INTO family(family_chore_fk, family_parent_fk, family_account_fk) VALUES (:family_chore_fk, :family_parent_fk, :familiy_account_fk);');
-        $stmtFamily->bindValue(':family_chore_fk',   $chore_pk, PDO::PARAM_INT);
-        $stmtFamily->bindValue(':family_parent_fk',  $parent_pk, PDO::PARAM_INT);
-        $stmtFamily->bindValue(':family_account_fk', $account_pk, PDO::PARAM_INT);
-        $stmtFamily->execute();
+        $stmt = $db->prepare('INSERT INTO family(family_chore_fk, family_parent_fk, family_account_fk) VALUES (:family_chore_fk, :family_parent_fk, :family_account_fk);');
+        $stmt->bindValue(':family_chore_fk', $chore_pk, PDO::PARAM_INT);
+        $stmt->bindValue(':family_parent_fk', $parent_pk, PDO::PARAM_INT);
+        $stmt->bindValue(':family_account_fk', $account_pk, PDO::PARAM_INT);
+        $stmt->execute();
         
         $count++;
         $_SESSION['test2'] .= $count;
-    }
+    }   
 }
     $_SESSION['test1'] = '$$chore_pk = ' . $chore_pk;
 
