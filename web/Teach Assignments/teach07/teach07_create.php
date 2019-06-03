57ff26948fdbe9fad3f01888fa94e0e621d07a43
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-06-03 16:19:36 -0700

    Teach07 first update

--- web/Teach Assignments/teach07/teach07_create.php
+++ web/Teach Assignments/teach07/teach07_create.php
@@ -1,6 +1,5 @@
 <?php
     session_start();
-    include'teach07_pass.php';
         
     if (!isset($_POST['userLogin_name']) || $_POST['userLogin_name'] == "" ||
         !isset($_POST['userLogin_pass']) || $_POST['userLogin_pass'] == "")
@@ -10,17 +9,16 @@
         header("Location: $signUp_page");
         die();
     }
+    include 'connectDB.php';
 
     $userLogin_name = htmlspecialchars($_POST['userLogin_name']);
     $userLogin_pass = $_POST['userLogin_pass'];
     $userLogin_Hpass = password_hash($userLogin_pass, PASSWORD_DEFAULT);
 
-    include 'connectDB.php';
-
     $stmt=$db->$prepare('INSERT INTO userLogin(userLogin_name, userLogin_pass)
             VALUES(:userLogin_name, :userLogin_pass)');
     $stmt->bindValue(':userLogin_name', $userLogin_name, PDO::PARAM_STR);
-    $stmt->bindValue(':userLogin_pass', $userLogin_Hpass, PDO::PARAM_STR);
+    $stmt->bindValue(':userLogin_pass', $userLogin_Hpass);
     $stmt->execute();
 
     // Re-direct to the sign-in page and kill this page
--- web/Teach Assignments/teach07/teach07_signOut.php
+++ web/Teach Assignments/teach07/teach07_signOut.php
@@ -1,6 +1,5 @@
 <?php
     session_start();
-    include 'teach07_pass.php';
     
     // remove all session variables
     session_unset();
