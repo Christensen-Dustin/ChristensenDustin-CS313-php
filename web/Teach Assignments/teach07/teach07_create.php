a1ca51297f1b60ea6fa03a47543462abd2e3aad0
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-06-03 12:48:15 -0700

    Teach07 initial upload

--- web/Teach Assignments/teach07/teach07_create.php
+++ web/Teach Assignments/teach07/teach07_create.php
@@ -6,8 +6,8 @@
         !isset($_POST['userLogin_pass']) || $_POST['userLogin_pass'] == "")
     {
         // Re-direct back to this page to enter in the required data
-        $create_page = "teach07_create.php";
-        header("Location: $create_page");
+        $signUp_page = "teach07_signUp.php";
+        header("Location: $signUp_page");
         die();
     }
 
@@ -28,32 +28,3 @@
     header("location: $signIn_page");
     die();
 ?>
-<!DOCTYPE html> 
-<html lang="eng-US">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Create a Username and Password</title>
-	<link rel="stylesheet" type="text/css" href="index.css">
-	<script type="text/javascript" src="name of file.js"></script>
-	<style></style>
-</head>  
-<body>
-<header>
-<?php
-    include 'navigationBar.php';
-?>
-</header>
-    <br>
-    <br>
-<h1>Please fill in the below information to create an account</h1>
-<form method="post" action="teach07_create.php">
-    <input type="text" name="userLogin_name" placeholder="User's Name">
-    <br>
-    <br>
-    <input type="password" name="userLogin_pass" placeholder="User's Password">
-    <br>
-    <br>
-    <input type="submit" value="Click to Create an Account"/>
-</form>    
-</body>
-</html>
---
+++ web/Teach Assignments/teach07/teach07_signUp.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html> 
+<html lang="eng-US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Create a Username and Password</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<script type="text/javascript" src="name of file.js"></script>
+	<style></style>
+</head>  
+<body>
+<header>
+<?php
+    include 'navigationBar.php';
+?>
+</header>
+    <br>
+    <br>
+<h1>Please fill in the below information to create an account</h1>
+<form method="post" action="teach07_create.php">
+    <input type="text" name="userLogin_name" placeholder="User's Name">
+    <br>
+    <br>
+    <input type="password" name="userLogin_pass" placeholder="User's Password">
+    <br>
+    <br>
+    <input type="submit" value="Click to Create an Account"/>
+</form>    
+</body>
+</html>
