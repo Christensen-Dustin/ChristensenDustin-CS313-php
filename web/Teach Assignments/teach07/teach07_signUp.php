39d6006ccec79ff6069a5966e3ef43d076ebbbdf
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-06-04 21:37:40 -0700

    Teach 07 update create

--- web/Teach Assignments/teach07/teach07_signUp.php
+++ web/Teach Assignments/teach07/teach07_signUp.php
@@ -7,7 +7,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Create a Username and Password</title>
 	<link rel="stylesheet" type="text/css" href="index.css">
-	<script type="text/javascript" src="name of file.js"></script>
+	<script type="text/javascript" src="Teach07.js"></script>
 	<style></style>
 </head>  
 <body>
@@ -19,21 +19,24 @@
     <br>
     <br>
 <h1>Please fill in the below information to create an account</h1>
-<form method="post" action="teach07_create.php">
+<form onsubmit="return validateDetails()" method="post" action="teach07_create.php">
     Enter a desired username:<br>
     <input type="text" name="userlogin_name" placeholder="User's Name"/>
     <br>
     <br>
     Enter a desired password:<br>
+<span class='asterisk' style="color:red">
 <?php
     if(isset($_SESSION['passMatch']) && $_SESSION['passMatch'] == false)
     {
         echo '<span style="color:red">*</span>';
     }
 ?>
+</span>
     <input type="password" name="userlogin_pass" placeholder="User's Password"/>
     <br>
     <br>
+<span class='asterisk' style="color:red">
 <?php
     if(isset($_SESSION['passMatch']) && $_SESSION['passMatch'] == false)
     {
@@ -42,18 +45,23 @@
 ?>
     <input type="password" name="userlogin_pass2" placeholder="User's Password"/>
     <br>
+</span>
+<span class='asterisk' style="color:red">
 <?php
     if(isset($_SESSION['passMatch']) && $_SESSION['passMatch'] == false)
     {
-        echo '<br><span style="color:red">Passwords must MATCH</span><br>';
+        echo '<br>Passwords must MATCH</span><br>';
     }
 ?>
+</span>
+<span id='message' style="color:red">
 <?php
     if(isset($_SESSION['passValid']) && $_SESSION['passValid'] == false)
     {
-        echo '<br><span style="color:red">Password must contain at least 7 characters and a number</span><br>';
+        echo '<br>Password must contain at least 7 characters and a number<br>';
     }
 ?>
+</span>
     <br>
     <input type="submit" value="Click to Create an Account"/>
 </form>    
