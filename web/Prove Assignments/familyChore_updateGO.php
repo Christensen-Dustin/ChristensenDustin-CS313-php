f12e2ef5d87d5253f184859371a2cba794e018df
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-06-05 20:23:26 -0700

    Family Chore Tracker Update items

--- web/Prove Assignments/familyChore_updateGO.php
+++ web/Prove Assignments/familyChore_updateGO.php
@@ -41,7 +41,7 @@ foreach ($db->query("select goal_pk, goal_name, goal_details, goal_date from goa
 {
     echo 'Goal Name: <input type="text" name="updateName" value="' . $rowGoal['goal_name'] . '"/><br>';
     echo 'Due Date : <input type="text" name="updateDate value="' . $rowGoal['goal_date'] . '"/><br>';
-    echo 'Goal Details: <textarea name="updateDetail" value="' . $rowGoal['goal_details'] . '"></textarea><br>';
+    echo 'Goal Details: <textarea name="updateDetail" >' . stripcslashes($rowGoal['goal_details']) . '</textarea><br>';
 }
 ?>
     </div>   
