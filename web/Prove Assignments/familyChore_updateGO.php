d5415b0da7634f7e96329f5834a636e1f5fe369e
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-06-05 21:36:54 -0700

    Family Chore Tracker Update Goal

--- web/Prove Assignments/familyChore_updateGO.php
+++ web/Prove Assignments/familyChore_updateGO.php
@@ -52,9 +52,9 @@ foreach ($db->query("select steps_details from goal inner join goalSteps on goal
 {
     echo '<input type="checkbox" name="updateSteps1[]" checked/>' . $rowChecked['steps_details'] . '</br>';
 }
-foreach ($db->query("select steps_details from steps") as $rowUnchecked)
+foreach ($db->query("select steps_details from goal inner join goalSteps on goal_pk = goalSteps_goal_fk inner join steps on goalSteps_steps_fk = steps_pk where goal_pk = '$goals'") as $rowChecked)
 {
-    foreach ($db->query("select steps_details from goal inner join goalSteps on goal_pk = goalSteps_goal_fk inner join steps on goalSteps_steps_fk = steps_pk where goal_pk = '$goals'") as $rowChecked)
+    foreach ($db->query("select steps_details from steps") as $rowUnchecked)
     {
         if($rowUnchecked['steps_details'] == $rowChecked['steps_details'])
         {
